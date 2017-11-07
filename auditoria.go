c9cd140afa866b9a03003328ecfe387f0a7a100a
Javier Reyes <ingjsreyesm@gmail.com>
2017-11-07 17:53:38 -0500

    Librería que ejecuta el cliente del elasticSearch, logstash para recibir los logs correspondientes

--- auditoria.go
+++ auditoria.go
@@ -1,49 +1,94 @@
-package Auditoria
+package main
 
 import (
-	//Se importan las librerias necesarias para que el Elastic funcione
 	"github.com/Sirupsen/logrus"
-	//"gopkg.in/olivere/elastic.v3"
-	//"gopkg.in/sohlich/elogrus.v1"
+	elastic "gopkg.in/olivere/elastic.v3"
+	elogrus "gopkg.in/sohlich/elogrus.v1"
 	//"os"
+	"log"
+	"net/http"
 	"time"
+	//"gopkg.in/olivere/elastic.v3"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gorilla/mux"
 )
 
-// func main() {
-// 	//Se coloca un tick para observar notificaciones al correr el programa
-// 	t := time.Tick(2 * time.Second)
-//
-// 	//Imprime la dirección del Elastic, ya que fue encapsulada como Variable de Entorno
-// 	println(os.Getenv("ELASTICSEARCH_URL"))
-// 	println("Imprimio la url")
-//
-// 	//Configurando el cliente del Elastic Search
-// 	client, err := elastic.NewSimpleClient(elastic.SetURL("http://127.0.0.1:9200"))
-// 	if err != nil {
-// 		panic("error elasticsearch " + err.Error())
-// 	}
-// 	println("cliente elastic creado")
-// 	println(client)
-//
-// 	//Se crea un hook
-// 	hook, err := elogrus.NewElasticHook(client, "loghost", logrus.DebugLevel, "logstash")
-// 	if err != nil {
-// 		panic("error elogrus" + err.Error())
-// 	}
-// 	println("hook elogrus creado")
-//
-// 	//Se agrega un hook al logrus
-// 	logrus.AddHook(hook)
-// }
+type cuerpo struct {
+	Id          int    `json:"id"`
+	Nombre      string `json:"nombre"`
+	Descripcion string `json:"descripcion"`
+}
 
-//Se crea un bucle for, teniendo en cuenta el tick que se realiza al principio, y muestra la fecha y la hora actual
-func insertar_elastic2(usuario string, peticion string) {
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	// url := r.URL.Path
+	params := mux.Vars(r)
+	tabla := params["tabla"]
+	id := params["id"]
+	//strB, _ := json.Marshal("gopher")
+
+	//Leer Cuerpo
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// Unmarshal
+	var msg *cuerpo
+	err = json.Unmarshal(b, &msg)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	fmt.Println(msg)
+
+	//println(r.Body["Nombre"]);
+
+	Insertar_elastic(tabla+" id:"+id, r.Method, msg)
+}
+
+func exec_cliente_elastic() {
+
+	//Configurando el cliente del Elastic Search
+	client, err := elastic.NewSimpleClient(elastic.SetURL("http://10.20.2.111:9200"))
+	if err != nil {
+		panic("error elasticsearch " + err.Error())
+	}
+	println("cliente elastic creado")
+	println(client)
+
+	//Se crea un hook
+	hook, err := elogrus.NewElasticHook(client, "loghost", logrus.DebugLevel, "logstash")
+	if err != nil {
+		panic("error elogrus" + err.Error())
+	}
+	println("hook elogrus creado")
+
+	//Se agrega un hook al logrus
+	logrus.AddHook(hook)
+
+	//Muxer
+	r := mux.NewRouter()
+
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/{tabla:[a-zA-Z0-9_]+}/{id:[a-zA-Z0-9_]+}", YourHandler)
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func Insertar_elastic(usuario string, peticion string, data *cuerpo) {
 	now := time.Now().String()
 	logrus.WithFields(logrus.Fields{
 		"Fecha_operacion": now,
-		"Usuario":         "prueba",
+		"Usuario":         usuario,
 		"Ip":              "10.20.0.256",
-		"Valor":           "test",
-		"Petición":        "GET",
+		"Petición":        peticion,
+		"Data":            data,
 	}).Info("Now!")
 }
